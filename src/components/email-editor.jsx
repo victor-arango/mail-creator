137a8b782638955415e1ba9cb5bd2ecdfb483da3
Victor Arango Quiroz <victor.arangoq@cadena.com.co>
2024-05-06 14:10:55 -0500

    add functions

--- src/components/email-editor.jsx
+++ src/components/email-editor.jsx
@@ -1,42 +1,73 @@
 import React, { useRef } from 'react';
 import { render } from 'react-dom'
-import template from '../../public/template.json'; // Adjust the path as needed
+import template from '/public/template.json'; // Adjust the path as needed
 
 import EmailEditor from 'react-email-editor';
 
 const App = (props) => {
   const emailEditorRef = useRef(null);
-
+ 
   const exportHtml = () => {
     emailEditorRef.current.editor.exportHtml((data) => {
       const { design, html } = data;
-      console.log('exportHtml', html);
+  
+      // Get today's date and format it (YYYY-MM-DD)
+      const today = new Date().toISOString().slice(0, 10);
+  
+      // Construct the desired filename
+      const filename = `plantilla-${today}.html`;
+  
+      const element = document.createElement("a");
+      const file = new Blob([html], { type: 'text/html' }); // Use text/html for HTML content
+  
+      element.href = URL.createObjectURL(file);
+      element.download = filename;
+      element.click();
     });
   };
 
   const onLoad = () => {
-    // const json = template
-    // console.log(json);
-    // this.editor.loadDesign(json)
+    const json = template
+    console.log(json);
+    emailEditorRef.current.editor.loadDesign(json)
   }
   
-
+  
   const onReady = () => {
     // editor is ready
     console.log('onReady');
   };
-
+  
   return (
-    <div>
-    
+    <div className='w-full h-screen' >
+      <button className='text-sm font-semibold leading-6 text-gray-900'  onClick={exportHtml}>Export HTML</button> 
       <EmailEditor
         ref={emailEditorRef}
         onLoad={onLoad}
         onReady={onReady}
+        minHeight="100%"
+        options={{
+        
+          customCSS: [
+            `
+                .tab-pane {
+                  background-color: white;
+                }
+                .nav-tabs {
+                  background-color: #6E43E0;
+                }
+                .nav-link.active {
+                  background-color: #AA89FF !important;
+                }
+                .nav-name, .nav-icon {
+                  color: white;
+                }
+              `,
+          ],
+        }}
       />
     </div>
   );
 };
 
 export default App;
-{/* <button onClick={exportHtml}>Export HTML</button> */}
