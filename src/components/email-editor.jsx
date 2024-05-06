48fd026728c61d120ab7a9b51b661e9d533f70d8
Victor Arango Quiroz <victor.arangoq@cadena.com.co>
2024-05-06 15:18:36 -0500

    style editor

--- src/components/email-editor.jsx
+++ src/components/email-editor.jsx
@@ -11,12 +11,8 @@ const App = (props) => {
     emailEditorRef.current.editor.exportHtml((data) => {
       const { design, html } = data;
   
-      // Get today's date and format it (YYYY-MM-DD)
       const today = new Date().toISOString().slice(0, 10);
-  
-      // Construct the desired filename
       const filename = `plantilla-${today}.html`;
-  
       const element = document.createElement("a");
       const file = new Blob([html], { type: 'text/html' }); // Use text/html for HTML content
   
@@ -40,14 +36,22 @@ const App = (props) => {
   
   return (
     <div className='w-full h-screen' >
-      <button className='text-sm font-semibold leading-6 text-gray-900'  onClick={exportHtml}>Export HTML</button> 
+       
+      <button className='text-sm font-semibold leading-6 text-gray-900'  onClick={exportHtml}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-download" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+  <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+  <path d="M12 17v-6" />
+  <path d="M9.5 14.5l2.5 2.5l2.5 -2.5" />
+</svg></button> 
       <EmailEditor
         ref={emailEditorRef}
         onLoad={onLoad}
         onReady={onReady}
         minHeight="100%"
-        options={{
+
         
+        options={{
           customCSS: [
             `
                 .tab-pane {
@@ -63,7 +67,10 @@ const App = (props) => {
                   color: white;
                 }
               `,
+              
+             
           ],
+          
         }}
       />
     </div>
