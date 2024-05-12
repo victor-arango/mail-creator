4d28cedecb25fd1cc99a6a5dbcd246db18e1109e
Victor Arango Quiroz <victor.arangoq@cadena.com.co>
2024-05-11 19:01:56 -0500

    add Adapter for SSR

--- src/components/email-editor.jsx
+++ src/components/email-editor.jsx
@@ -54,14 +54,14 @@ const App = (props) => {
   return (
     <div className='w-full h-screen' >
       <div className='w-full flex justify-end pr-10'>
-      <button className='text-sm font-semibold leading-6 text-gray-900'  onClick={Save}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-download" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      {/* <button className='text-sm font-semibold leading-6 text-gray-900'  onClick={Save}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-download" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M14 3v4a1 1 0 0 0 1 1h4" />
   <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
   <path d="M12 17v-6" />
   <path d="M9.5 14.5l2.5 2.5l2.5 -2.5" />
 </svg>
-</button> 
+</button>  */}
       <button className='text-sm font-semibold leading-6 text-gray-900'  onClick={exportHtml}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-download" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M14 3v4a1 1 0 0 0 1 1h4" />
